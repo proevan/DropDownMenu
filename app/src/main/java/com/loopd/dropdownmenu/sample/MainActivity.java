a62073a47cf5e5419dfa77a5d438b576049cd9d6
proevan <yanghsin.lin@gmail.com>
2015-10-09 14:57:13 -0700

    Initial commit

---
+++ app/src/main/java/com/loopd/dropdownmenu/sample/MainActivity.java
@@ -0,0 +1,50 @@
+package com.loopd.dropdownmenu.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.loopd.dropdownmenu.DropDownMenu;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mDropDownButton;
+    private com.loopd.dropdownmenu.DropDownMenu mDropDownMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDropDownMenu = (DropDownMenu) findViewById(R.id.drop_down_menu);
+        mDropDownButton = (ImageView) findViewById(R.id.drop_down_btn);
+        mDropDownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDropDownButton.setVisibility(View.INVISIBLE);
+                mDropDownMenu.open();
+            }
+        });
+        initDropDownMenu();
+    }
+
+    private void initDropDownMenu() {
+        mDropDownMenu.setCloseButtonDrawable(R.drawable.drop_down_btn);
+        mDropDownMenu.addMenuButton("button1", R.drawable.drop_down_btn);
+        mDropDownMenu.addMenuButton("button2", R.drawable.drop_down_btn);
+        mDropDownMenu.addMenuButton("button3", R.drawable.drop_down_btn);
+        mDropDownMenu.setOnMenuCollapsedListener(new DropDownMenu.OnMenuCollapsedListener() {
+            @Override
+            public void onCollapsed() {
+                mDropDownButton.setVisibility(View.VISIBLE);
+            }
+        });
+        mDropDownMenu.setOnMenuButtonClickListener(new DropDownMenu.OnMenuButtonClickListener() {
+            @Override
+            public void onMenuButtonClick(int position) {
+                Toast.makeText(MainActivity.this, "click: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ lib/src/main/java/com/loopd/dropdownmenu/DropDownMenu.java
@@ -0,0 +1,170 @@
+package com.loopd.dropdownmenu;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.daimajia.easing.Glider;
+import com.daimajia.easing.Skill;
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ObjectAnimator;
+
+public class DropDownMenu extends FrameLayout {
+    private static final int SLIDING_MENU_SLIDING_DURATION = 400;
+    private Context mContext;
+    private LinearLayout mMenuButtonsLayout;
+    private ImageView mCloseButton;
+    private OnMenuCollapsedListener mOnMenuCollapsedListener;
+    private OnMenuButtonClickListener mOnMenuButtonClickListener;
+    private boolean mAnimationLock = false;
+
+    public DropDownMenu(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public DropDownMenu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public DropDownMenu(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        mContext = context;
+        View dropDownMenuLayout = LayoutInflater.from(context).inflate(R.layout.drop_down_menu, null);
+        mCloseButton = (ImageView) dropDownMenuLayout.findViewById(R.id.close_btn);
+        mMenuButtonsLayout = (LinearLayout) dropDownMenuLayout.findViewById(R.id.menu_buttons_layout);
+        initCloseButtonListener();
+        addView(dropDownMenuLayout);
+        setVisibility(INVISIBLE);
+    }
+
+    private void initCloseButtonListener() {
+        mCloseButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mAnimationLock) {
+                    mAnimationLock = true;
+                    AnimatorSet slideAnimationSet = new AnimatorSet();
+                    slideAnimationSet.playTogether(
+                            Glider.glide(Skill.CubicEaseOut, SLIDING_MENU_SLIDING_DURATION, ObjectAnimator.ofFloat(mMenuButtonsLayout, "translationY", 0, -mMenuButtonsLayout.getHeight())),
+                            Glider.glide(Skill.CubicEaseOut, SLIDING_MENU_SLIDING_DURATION, ObjectAnimator.ofFloat(mCloseButton, "rotation", -180, 0))
+                    );
+                    slideAnimationSet.addListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            setVisibility(GONE);
+                            if (mOnMenuCollapsedListener != null) {
+                                mOnMenuCollapsedListener.onCollapsed();
+                            }
+                            mAnimationLock = false;
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+                        }
+                    });
+                    slideAnimationSet.setDuration(SLIDING_MENU_SLIDING_DURATION);
+                    slideAnimationSet.start();
+                }
+            }
+        });
+    }
+
+    public void setGravity(int gravity) {
+        mMenuButtonsLayout.setGravity(gravity);
+    }
+
+    public void setCloseButtonDrawable(int drawableRes) {
+        mCloseButton.setImageResource(drawableRes);
+    }
+
+    public void addMenuButton(String title, int drawableRes) {
+        View menuButton = inflateMenuButton(title, drawableRes);
+        final int buttonPosition = mMenuButtonsLayout.getChildCount();
+        menuButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mOnMenuButtonClickListener != null) {
+                    mOnMenuButtonClickListener.onMenuButtonClick(buttonPosition);
+                }
+            }
+        });
+        mMenuButtonsLayout.addView(menuButton, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f));
+    }
+
+    private View inflateMenuButton(String title, int drawableRes) {
+        View dropDownButton = LayoutInflater.from(mContext).inflate(R.layout.drop_down_button, null);
+        ImageView iconImageView = (ImageView) dropDownButton.findViewById(R.id.icon);
+        TextView titleTextView = (TextView) dropDownButton.findViewById(R.id.title);
+        iconImageView.setImageResource(drawableRes);
+        titleTextView.setText(title);
+        return dropDownButton;
+    }
+
+    public void open() {
+        if (!mAnimationLock) {
+            mAnimationLock = true;
+            AnimatorSet slideAnimationSet = new AnimatorSet();
+            slideAnimationSet.playTogether(
+                    Glider.glide(Skill.CubicEaseOut, SLIDING_MENU_SLIDING_DURATION, ObjectAnimator.ofFloat(mMenuButtonsLayout, "translationY", -mMenuButtonsLayout.getHeight(), 0)),
+                    Glider.glide(Skill.CubicEaseOut, SLIDING_MENU_SLIDING_DURATION, ObjectAnimator.ofFloat(mCloseButton, "rotation", 0, -180))
+            );
+            slideAnimationSet.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    setVisibility(VISIBLE);
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mAnimationLock = false;
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+                }
+            });
+            slideAnimationSet.setDuration(SLIDING_MENU_SLIDING_DURATION);
+            slideAnimationSet.start();
+        }
+    }
+
+    public void setOnMenuCollapsedListener(OnMenuCollapsedListener callback) {
+        mOnMenuCollapsedListener = callback;
+    }
+
+    public void setOnMenuButtonClickListener(OnMenuButtonClickListener callback) {
+        mOnMenuButtonClickListener = callback;
+    }
+
+    public interface OnMenuCollapsedListener {
+        void onCollapsed();
+    }
+
+    public interface OnMenuButtonClickListener {
+        void onMenuButtonClick(int position);
+    }
+}
